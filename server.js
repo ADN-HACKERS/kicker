0fc6eb3944d775cc7101878d46e1abba7db35012
unknown <jbelinouha9@gmail.com>
2020-12-12 05:22:00 -0800

    sign in authentication

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express=require('express');
 require('dotenv').config({path: './config/.env'});
 const userRoutes=require('./routes/user.routes')
 const postRoutes=require('./routes/post.routes')
+const cors=require('cors')
 const bodyParser=require('body-parser')
 const cookieParser=require('cookie-parser');
 const {checkUser,requireAuth}= require('./middleware/auth.middleware')
 require('./config/db')
+
 const app = express();
 
+const corsOptions={
+  origin :'http://localhost:3001',
+  credentials :true,
+  'allowedHeaders':['sessionId','Content-Type'],
+  'exposedMethods':['sessionId'],
+  'methods':'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue':false
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cookieParser())
