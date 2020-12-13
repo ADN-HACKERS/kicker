4e7d5b7c514d1e8eaf61607b728f0b8d86617039
DhiaDhafer <dhiadhaferr@gmail.com>
2020-12-13 18:19:28 +0100

    test

--- client/src/components/Log/index.js
+++ client/src/components/Log/index.js
@@ -1,39 +1,44 @@
-import React,{useState} from 'react';
-import SignUpForm from './SignUpForm'
-import SignInForm from './SignInForm'
+import React, { useState } from "react";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
-const Log=(props)=>{
+const Log = (props) => {
+  const [SignUpModal, setSignUpModal] = useState(props.signup);
+  const [SignInModal, setSignInModal] = useState(props.signin);
 
-const [SignUpModal ,setSignUpModal]=useState(props.signup)
-const [SignInModal ,setSignInModal]=useState(props.signin)
-
-const handleModals=(e)=>{
-
-  if(e.target.id === "register"){
-    setSignInModal(false)
-    setSignUpModal(true)
-  }else if(e.target.id === 'login'){
-    setSignUpModal(false)
-    setSignInModal(true)
-
-  }
-}
-
+  const handleModals = (e) => {
+    if (e.target.id === "register") {
+      setSignInModal(false);
+      setSignUpModal(true);
+    } else if (e.target.id === "login") {
+      setSignUpModal(false);
+      setSignInModal(true);
+      //hello test
+    }
+  };
 
   return (
-    <div  >
-      <div > 
-      <ul>
-        <li onClick={handleModals} id="register" className={SignUpModal ? "active-btn" :null}>Sign Up</li>
-        <li onClick={handleModals} id="login"  className={SignInModal ? "active-btn" :null}> Sign In</li>
-      </ul>
-         {SignUpModal && <SignUpForm />}
-         {SignInModal && <SignInForm />}
-
+    <div>
+      <div>
+        <ul>
+          <li
+            onClick={handleModals}
+            id="register"
+            className={SignUpModal ? "active-btn" : null}>
+            Sign Up
+          </li>
+          <li
+            onClick={handleModals}
+            id="login"
+            className={SignInModal ? "active-btn" : null}>
+            {" "}
+            Sign In
+          </li>
+        </ul>
+        {SignUpModal && <SignUpForm />}
+        {SignInModal && <SignInForm />}
       </div>
-      
-    
     </div>
-  )
-}
+  );
+};
 export default Log;
